c0aa3bb8decdc002a7bb44f66100dae87fa91cdd
Pedro Sousa <pedropintodsousa@gmail.com>
2025-02-09 11:15:22 +0000

    Changed Kafka to not use Zookeeper and use KRaft instead

--- kafka/create-topics.sh
+++ kafka/create-topics.sh
@@ -7,7 +7,10 @@ check_kafka_connection() {
   kafka-topics --bootstrap-server "$KAFKA_BROKER" --list > /dev/null 2>&1
 }
 
-# Wait until Kafka is ready
+topic_exists() {
+  kafka-topics --bootstrap-server "$KAFKA_BROKER" --describe --topic "$1" > /dev/null 2>&1
+}
+
 while ! check_kafka_connection; do
   echo "$(date '+%Y-%m-%d %H:%M:%S') - Kafka is not ready, waiting 5 seconds..."
   sleep 5
@@ -15,11 +18,14 @@ done
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Kafka is ready, starting topic creation"
 
-# Create topics
 for topic in "${TOPICS[@]}"; do
-  echo "Creating topic '$topic'..."
-  kafka-topics --bootstrap-server "$KAFKA_BROKER" --create --topic "$topic" --partitions 1 --replication-factor 1
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Topic '$topic' created"
+  if topic_exists "$topic"; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Topic '$topic' already exists, skipping creation"
+  else
+    echo "Creating topic '$topic'..."
+    kafka-topics --bootstrap-server "$KAFKA_BROKER" --create --topic "$topic" --partitions 1 --replication-factor 1
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Topic '$topic' created"
+  fi
   sleep 2
 done
 
