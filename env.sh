5411959de99bfa08e76c004f83f511dcf492d73a
Pedro Sousa <pedropintodsousa@gmail.com>
2025-02-12 21:40:02 +0000

    Valkey added

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Define the Compose file list as a global variable
-COMPOSE_FILE="dc-networks.yml:dc-kafka-yml:dc-kafka-tools.yml:dc-minio.yml:dc-postgres.sql"
+COMPOSE_FILE="dc-networks.yml:dc-kafka-yml:dc-kafka-tools.yml:dc-minio.yml:dc-postgres.sql:dc-valkey.yml"
 
 export COMPOSE_FILE
--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ declare -a SERVICE_NAMES=(
   "Kafka Tools"
   "PostgreSQL"
   "MinIO"
+  "Valkey"
 )
 
 declare -a SERVICE_FILES=(
@@ -13,6 +14,7 @@ declare -a SERVICE_FILES=(
   "dc-kafka-tools.yml"
   "dc-postgres.yml"
   "dc-minio.yml"
+  "dc-valkey.yml"
 )
 
 # Define which services should be selected by default (using indices)
